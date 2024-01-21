75b94f8b494e616c7e2b022d94f42c7558eeafc2
Aliireza 1101 <aliireza.dev@gmail.com>
2024-01-21 19:16:28 +0330

    Update

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.index_title = "Site admin"
+admin.site.site_title = "Administration"
+admin.site.site_header = "Administration"
+admin.site.site_url = "kfdkasjhfj"
