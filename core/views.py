c0e4fa3825abde38a858e60a40effb1b34a52fe9
Aliireza 1101 <aliireza.dev@gmail.com>
2024-01-24 22:24:52 +0330

    Dashboard search using trigram similarity

--- core/views.py
+++ core/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest
 from django.contrib.auth.decorators import login_required
-from . models import Examinee
+from django.contrib.postgres.search import TrigramSimilarity
+from django.db.models import QuerySet
+
+from .models import Examinee
+
 
 # Create your views here.
 @login_required
 def dashboard(request: HttpRequest):
-    examinees = Examinee.objects.all()
-    context = {'examinees': examinees}
+    query = request.GET.get("query")
+    if query:
+        examinees = Examinee.objects.annotate(
+            similarity=TrigramSimilarity("name", query)
+        ).filter(similarity__gt=0.05)
+    else:
+        examinees = Examinee.objects.all().order_by("-date")
+
+    context = {"examinees": examinees}
     return render(request, "dashboard/index.html", context)
