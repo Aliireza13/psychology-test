4f77ceafc5cb10bd11ce1152ef538ae2706a2c49
Aliireza 1101 <aliireza.dev@gmail.com>
2024-01-24 19:30:41 +0330

    Update Dashboard: Users list

--- core/views.py
+++ core/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.contrib.auth.decorators import login_required
-
+from . models import Examinee
 
 # Create your views here.
 @login_required
 def dashboard(request: HttpRequest):
-    context = {}
-    return render(request, "dashboard/index.html")
+    examinees = Examinee.objects.all()
+    context = {'examinees': examinees}
+    return render(request, "dashboard/index.html", context)
