120c0f87eba824db229904edb36f5679bb8f72d2
Aliireza 1101 <aliireza.dev@gmail.com>
2024-05-17 12:58:30 +0330

    collect static | migration

--- psychology_test/settings.py
+++ psychology_test/settings.py
@@ -154,7 +154,7 @@ if config("DEBUG", cast=bool):
         os.path.join(BASE_DIR, "static"),
     ]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
